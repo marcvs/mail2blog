575a35b4eaaebe9c2157bb8a2f297ecaef1e8876
Marcus <marcus@hardt-it.de>
2021-07-19 22:10:20 +0200

    use more templates

--- mail2blog/view.py
+++ mail2blog/view.py
@@ -15,9 +15,11 @@ import sys
 import os
 import re
 import subprocess
+from jinja2 import Template
 
 from mail2blog import logsetup
 from mail2blog import tools 
+from mail2blog.parse_args import args
 from mail2blog.config import CONFIG
 from mail2blog.database import Blog_entry, Blog
 
@@ -43,6 +45,10 @@ class ArticleRenderer():
 
         # If the html file is already there, we don't need to re-render:
         if os.path.exists(self.html_output_file):
+            if args.force:
+                logger.info(F"{self.subject} already exists => removing article")
+                os.remove(self.html_output_file)
+        if os.path.exists(self.html_output_file):
             logger.info(F"{self.subject} already exists => skipping article")
 
         else:
@@ -54,32 +60,60 @@ class ArticleRenderer():
             self.media = []
 
             self.walker()
-            if self.media_part_found:
+            if self.media_part_found and args.nopix is True:
                 self._run_bic()
                 self._add_gallery_url()
             self.write_output()
 
     def write_output(self):
-        '''render md to html and write output'''
-        html_data = tools.render_pandoc_with_theme(self.markdown)
+        '''render:
+            - md message via jinja to md
+            - md to html 
+            and write output'''
+        # collect data:
+        template_file     = CONFIG.get('templates', 'article')
+        date              = self.blog_entry.date
+        subject           = self.blog_entry.subject
+        subject_no_spaces = self.blog_entry.get_subject(replace_spaces=True)
+        author            = self.blog_entry.author
+        author_email      = self.blog_entry.author_email
+        message_id        = self.blog_entry.message_id
+        
+        # render template
+        with open(template_file, 'r') as fh:
+            template_data = fh.read()
+        template = Template(template_data)
+
+        markdown_data = template.render(date = date, 
+                                subject = subject,
+                                author = author,
+                                author_first = author.split(' ')[0],
+                                author_last  = author.split(' ')[1],
+                                author_email = author_email, 
+                                content = self.markdown,
+                                link = F"{subject_no_spaces}-{self.message_id}.html")
+
+        html_data = tools.render_pandoc_with_theme(markdown_data, title=subject)
+
         logger.debug(F"saving html to {self.html_output_file}")
         with open(self.html_output_file, 'w') as fp:
             fp.write(html_data)
         os.chmod(self.html_output_file, 0o644)
 
-    def render(self, maintype, *args, **kwargs):
+    def render(self, maintype, *myargs, **mykwargs):
         if maintype == "text":
-            return(self.text_renderer(*args, **kwargs))
-        if maintype == "image":
-            return(self.image_renderer(*args, **kwargs))
-        if maintype == "video":
-            return(self.video_renderer(*args, **kwargs))
+            return(self.text_renderer(*myargs, **mykwargs))
+        if maintype == "image" and not args.nopix:
+            return(self.image_renderer(*myargs, **mykwargs))
+        if maintype == "video" and not args.nopix:
+            return(self.video_renderer(*myargs, **mykwargs))
         return None
 
     def text_renderer(self, part):
         prt = part.get_payload(decode=True)
         self.markdown = prt.decode('iso-8859-1')
         self.markdown = self.markdown.split('\n-- ')[0]
+
     def image_renderer(self, part):
         JPEG_EXTENSIONS=['JPG', 'JPEG', 'Jpeg', 'jpeg']
         tools.makepath(self.media_output_dir, 2)
@@ -128,10 +162,10 @@ class ArticleRenderer():
 
     def _add_gallery_url(self):
         # FIXME: move this to template
-        gallery_link_base = CONFIG.get('locations', 'gallery_link_base') 
-        # self.markdown += F"\n[Bilder]({gallery_link_base}/{self.gallery_name})"
-        icon_url = F"{gallery_link_base}/{self.gallery_name}/gallery/thumbs/{self.gallery_icon_basename}"
-        self.markdown += F"[![icon]({icon_url})]({gallery_link_base}/{self.gallery_name})"
+        gallery_link_base = CONFIG.get('locations', 'gallery_link_base')
+        gallery_link      = F"{gallery_link_base}/{self.gallery_name}"
+        icon_url          = F"{gallery_link_base}/{self.gallery_name}/gallery/thumbs/{self.gallery_icon_basename}"
+        self.markdown    += F"[![icon]({icon_url})]({gallery_link})"
 def generate_index():
     blog=Blog()
     blog.read_entries_from_imap()
@@ -141,8 +175,7 @@ def generate_index():
 
     blog_index_md = blog.generate_index()
 
-    blog_index_html = tools.render_pandoc_with_theme(blog_index_md)
-    # FIXME: index.html
+    blog_index_html = tools.render_pandoc_with_theme(blog_index_md, title="Nordkapp Tour 2021")
     index_file_name = CONFIG.get('locations', 'blog_output') + "/index.html"
 
     tools.makepath(CONFIG.get('locations', 'blog_output'))
