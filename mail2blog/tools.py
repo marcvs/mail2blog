788d412cde0d4e9e7c1a8f9a9272109559b3330e
Marcus <marcus@hardt-it.de>
2021-09-25 16:31:59 +0200

    hardcode center of map

--- mail2blog/tools.py
+++ mail2blog/tools.py
@@ -61,7 +61,7 @@ def render_pandoc_with_geolocation (inpt, title="Title", gpx_data=False, geoloca
         </article>
 
         <script>
-            var mymap = L.map('mapid').setView([67.00, 9.49], 4);
+            var mymap = L.map('mapid').setView([45.00, 9.49], 7);
             L.tileLayer('https://api.mapbox.com/styles/v1/{{id}}/tiles/{{z}}/{{x}}/{{y}}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {{
                 maxZoom: 18,
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
